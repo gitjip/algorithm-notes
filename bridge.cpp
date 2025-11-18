a191ac60880b6aa105901eebbf9fd09508cb7593
gitjip <2672259086@qq.com>
2025-11-18 23:46:57 +0800

    add code

---
+++ bridge.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+// 图的邻接表
+vector<vector<int>> g;
+// 存储割边
+vector<pair<int, int>> e;
+vector<int> dfn, low;
+int tot;
+// tarjan算法求割边(桥)
+void tarjan(int u, int fa)
+{
+    dfn[u] = low[u] = tot++;
+    // 是否访问过连向父亲的边
+    bool fa_edge = false;
+    for (auto v : g[u])
+    {
+        if (dfn[v] == -1)
+        {
+            tarjan(v, u);
+            low[u] = min(low[u], low[v]);
+            if (low[v] > dfn[u])
+            // 发现割边
+                e.push_back({min(u, v), max(u, v)});
+        }
+        // 若目标节点不是父亲或访问到连向父亲的重边
+        else if (v != fa || fa_edge)
+            low[u] = min(low[u], dfn[v]);
+        else
+            fa_edge = true;
+    }
+}
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+    g.resize(n);
+    dfn.resize(n, -1);
+    low.resize(n, -1);
+    for (int i = 0; i < m; ++i)
+    {
+        int u, v;
+        cin >> u >> v;
+        u--;
+        v--;
+        g[u].push_back(v);
+        g[v].push_back(u);
+    }
+    for (int i = 0; i < n; ++i)
+        if (dfn[i] == -1)
+            tarjan(i, -1);
+    sort(e.begin(), e.end());
+    for (auto [u, v] : e)
+        cout << u + 1 << " " << v + 1 << "\n";
+    return 0;
+}
