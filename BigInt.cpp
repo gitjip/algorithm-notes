fe1a69e6953e1ab5ccecbcace32c08b3a4f37fc6
佘嘉雄 <2672259086@qq.com>
2026-03-10 14:38:15 +0800

    modify

--- BigInt.cpp
+++ BigInt.cpp
@@ -5,22 +5,20 @@ const int BASE = 1000000000;
 const int WIDTH = 9;
 struct BigInt {
     vector<ll> num;
-    bool neg;
-    BigInt()
-        : neg(false)
-    {
-    }
+    bool neg = false;
+    BigInt() { }
     BigInt(ll x)
     {
         neg = x < 0;
         x = std::abs(x);
-        if (x == 0)
+        if (x == 0) {
             num.push_back(0);
-        else
+        } else {
             while (x) {
                 num.push_back(x % BASE);
                 x /= BASE;
             }
+        }
     }
     BigInt(const string& s)
     {
@@ -34,36 +32,39 @@ struct BigInt {
                 val = val * 10 + s[j] - '0';
             num.push_back(val);
         }
-        if (num.empty())
+        if (num.empty()) {
             num.push_back(0);
+        }
         trim();
     }
     void trim()
     {
-        while (num.size() > 1 && num.back() == 0)
+        while (num.size() > 1 && num.back() == 0) {
             num.pop_back();
-        if (num.size() == 1 && num[0] == 0)
+        }
+        if (num.size() == 1 && num[0] == 0) {
             neg = false;
+        }
     }
     bool operator<(const BigInt& b) const
     {
-        if (neg != b.neg)
+        if (neg != b.neg) {
             return neg;
-        if (num.size() != b.num.size())
+        }
+        if (num.size() != b.num.size()) {
             return neg ^ (num.size() < b.num.size());
-        for (int i = num.size() - 1; i >= 0; i--) {
-            if (num[i] != b.num[i])
+        }
+        for (int i = (int)num.size() - 1; i >= 0; i--) {
+            if (num[i] != b.num[i]) {
                 return neg ^ (num[i] < b.num[i]);
+            }
         }
         return false;
     }
     bool operator>(const BigInt& b) const { return b < *this; }
     bool operator<=(const BigInt& b) const { return !(b < *this); }
     bool operator>=(const BigInt& b) const { return !(*this < b); }
-    bool operator==(const BigInt& b) const
-    {
-        return neg == b.neg && num == b.num;
-    }
+    bool operator==(const BigInt& b) const { return neg == b.neg && num == b.num; }
     bool operator!=(const BigInt& b) const { return !(*this == b); }
     BigInt abs() const
     {
@@ -76,12 +77,12 @@ struct BigInt {
         BigInt res;
         res.num.clear();
         ll carry = 0;
-        for (int i = 0; i < max(a.num.size(), b.num.size()) || carry; i++) {
+        for (int i = 0; i < (int)max(a.num.size(), b.num.size()) || carry; i++) {
             ll x = carry;
-            if (i < a.num.size()) {
+            if (i < (int)a.num.size()) {
                 x += a.num[i];
             }
-            if (i < b.num.size()) {
+            if (i < (int)b.num.size()) {
                 x += b.num[i];
             }
             res.num.push_back(x % BASE);
@@ -95,9 +96,9 @@ struct BigInt {
         BigInt res;
         res.num.clear();
         ll borrow = 0;
-        for (int i = 0; i < a.num.size(); i++) {
+        for (int i = 0; i < (int)a.num.size(); i++) {
             ll x = a.num[i] - borrow;
-            if (i < b.num.size()) {
+            if (i < (int)b.num.size()) {
                 x -= b.num[i];
             }
             borrow = x < 0;
@@ -135,10 +136,10 @@ struct BigInt {
     {
         BigInt res;
         res.num.assign(num.size() + b.num.size(), 0);
-        for (size_t i = 0; i < num.size(); ++i) {
+        for (int i = 0; i < (int)num.size(); ++i) {
             ll carry = 0;
-            for (size_t j = 0; j < b.num.size() || carry; ++j) {
-                ll cur = res.num[i + j] + num[i] * (j < b.num.size() ? b.num[j] : 0) + carry;
+            for (int j = 0; j < (int)b.num.size() || carry; ++j) {
+                ll cur = res.num[i + j] + num[i] * (j < (int)b.num.size() ? b.num[j] : 0) + carry;
                 res.num[i + j] = cur % BASE;
                 carry = cur / BASE;
             }
@@ -150,7 +151,8 @@ struct BigInt {
     friend pair<BigInt, BigInt> div_mod_abs(const BigInt& a, const BigInt& b)
     {
         // 竖式除法实现
-        if (b == BigInt(0)) throw runtime_error("division by zero");
+        if (b == BigInt(0))
+            throw runtime_error("division by zero");
         BigInt dividend = a.abs();
         BigInt divisor = b.abs();
         BigInt quotient, remainder;
@@ -176,7 +178,7 @@ struct BigInt {
         }
         quotient.trim();
         remainder.trim();
-        return {quotient, remainder};
+        return { quotient, remainder };
     }
     BigInt operator/(const BigInt& b) const
     {
@@ -197,21 +199,23 @@ struct BigInt {
     string to_string() const
     {
         string res;
-        if (neg)
+        if (neg) {
             res += '-';
+        }
         res += ::to_string(num.empty() ? 0 : num.back());
-        for (int i = num.size() - 2; i >= 0; i--) {
+        for (int i = (int)num.size() - 2; i >= 0; i--) {
             string s = ::to_string(num[i]);
-            res += string(WIDTH - s.size(), '0') + s;
+            res += string(WIDTH - (int)s.size(), '0') + s;
         }
         return res;
     }
     ll to_ll() const
     {
         ll res = 0;
-        for (int i = num.size() - 1; i >= 0; i--) {
-            if (res > (LLONG_MAX - num[i]) / BASE)
+        for (int i = (int)num.size() - 1; i >= 0; i--) {
+            if (res > (LLONG_MAX - num[i]) / BASE) {
                 throw overflow_error("to_ll overflow");
+            }
             res = res * BASE + num[i];
         }
         return neg ? -res : res;
@@ -229,27 +233,7 @@ struct BigInt {
         return out;
     }
 };
-// 测试样例
-int main()
+BigInt gcd(BigInt a, BigInt b)
 {
-    BigInt a, b;
-    cin >> a >> b;
-    cout << "a+b=" << a + b << endl;
-    cout << "a-b=" << a - b << endl;
-    cout << "a*b=" << a * b << endl;
-    if (b != BigInt(0)) {
-        cout << "a/b=" << a / b << endl;
-        cout << "a%b=" << a % b << endl;
-    }
-    cout << "a<b: " << (a < b) << endl;
-    cout << "a>b: " << (a > b) << endl;
-    cout << "a==b: " << (a == b) << endl;
-
-    BigInt c = 1234567890123456789LL;
-    cout << "c=" << c << endl;
-    cout << "c to ll: " << c.to_ll() << endl;
-
-    BigInt d = string("-9876543210987654321");
-    cout << "d=" << d << endl;
-    return 0;
+    return b == 0 ? a : gcd(b, a % b);
 }
